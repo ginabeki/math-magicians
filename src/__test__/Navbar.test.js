c4fe122df718f770f866e5cf49fbbb6f65d2b4ff
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-12-07 21:10:02 +0500

    Create test for the Navbar

--- src/__test__/Navbar.test.js
+++ src/__test__/Navbar.test.js
@@ -1,33 +1,17 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import { useEvent } from '@testing-library/user-event';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import Navbar from '../components/Navbar/Navbar';
 
-// test('Testing navigation', () => {
-//   render(
-//     <div className="navBar">
-//       <ul className="menu">
-//         <li>
-//           <a href="/" className="navItem">Home</a>
-//         </li>
-//         <li>
-//           <a href="/calculator" className="navItem">Calculator</a>
-//         </li>
-//         <li>
-//           <a href="/inspiration" className="navItem">Quote</a>
-//         </li>
-//       </ul>
-//     </div>,
-//   );
-//     const onChange = jest.fn()
-//     render(<input type="checkbox" onChange={onChange} />)
-//     const checkbox = screen.getByRole('checkbox')
-//     userEvent.dblClick(checkbox)
-//     expect(onChange).toHaveBeenCalledTimes(2)
-//     expect(checkbox).not.toBeChecked()
- 
-
-  
-// });
-
-// //   useEvent.click(screen.getByText('Calculator'));
-// //   useEvent.click(screen.getByText('Quote'));
+describe('Navbar component', () => {
+  it('should render Navbar component correctly', () => {
+    render(
+      <Router>
+        <Navbar />
+      </Router>,
+    );
+    const element = screen.getByRole('list');
+    expect(element).toBeInTheDocument();
+  });
+});
